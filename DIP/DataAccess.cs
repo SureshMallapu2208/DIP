90a327d9d845f81ba0404aa8ead65c6dd2aa6573
m.suresh <m.suresh@LTPBAN252476763>
2023-08-23 16:50:04 +0530

    Add project files.

---
+++ DIP/DataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+    internal class DataAccess: IRepository
+    {
+        public void Save()
+        {
+            //TODO: Need to implement
+
+            Console.WriteLine("Saved Successfully.");
+        }
+    }
+}
---
+++ DIP/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DIP;
+
+Console.WriteLine("Hello, World!");
+
+BusinessLogic obj = new BusinessLogic();
+obj.SaveDetails();
+
+Console.Read();
